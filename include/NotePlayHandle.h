575e36787bc2c2e90f11526a8986d0590bdb2d98
Hyunin Song <tteu.ingog@gmail.com>
2017-09-02 16:07:11 +0900

    Fix Env/LFO in case of tempo automation

--- include/NotePlayHandle.h
+++ include/NotePlayHandle.h
@@ -249,7 +249,7 @@ public:
 	void processTimePos(const TimePos& time, float pitchValue, bool isRecording);
 
 	/*! Updates total length (m_frames) depending on a new tempo */
-	void resize( const bpm_t newTempo );
+	void resize(const bpm_t newTempo);
 
 	/*! Set song-global offset (relative to containing MIDI clip) in order to properly perform the note detuning */
 	void setSongGlobalParentOffset( const TimePos& offset )
@@ -316,6 +316,7 @@ private:
 
 	// tempo reaction
 	bpm_t m_origTempo;						// original tempo
+	bpm_t m_curTempo;
 	f_cnt_t m_origFrames;					// original m_frames
 
 	int m_origBaseNote;
